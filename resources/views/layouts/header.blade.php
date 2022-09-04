2042c61d7975c287e1e3bfdd571253d4e621e9b8
Andrés Vargas <85952546+Skippy696@users.noreply.github.com>
2022-09-03 19:13:43 -0600

    i relize configurations with tailwind

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,31 @@
+<header>
+    <nav>
+        <div class="mx-auto bg-black min-h-screen">
+            <div class="flex justify-between">
+                <div class="flex space-x-4">
+                    <div>
+                        <a href="#"
+                            class="brand font-extrabold text-6xl flex items-center py-4 px-2 text-white ml-20 hover:text-pink-600">Andrés
+                            Vargas</a>
+                    </div>
+                </div>
+                <div class=" hidden md:flex items-center space-x-1 mr-52">
+                    <a href="#" class="py-4 px-2 text-xl text-white hover:text-pink-600">Home</a>
+                    <a href="#" class="py-4 px-2 text-xl text-white hover:text-pink-600">About</a>
+                    <a href="#" class="py-4 px-2 text-xl text-white hover:text-pink-600">Services</a>
+                    <a href="#" class="py-4 px-2 text-xl text-white hover:text-pink-600">Blog</a>
+                    <a href="#" class="py-4 px-2 text-xl text-white hover:text-pink-600">Contact</a>
+                </div>
+                <!--phone menu-->
+                <div class="md:hidden flex items-center">
+                    <button class="mobile-menu-button p-6 focus:outline-none">
+                        <svg class="w-9 h-9 text-white hover:text-pink-600" x-show="!showMenu" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+    </nav>
+</header>
---
+++ resources/views/page/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+@section('title', 'Home')
+@section('content')
+  <h1 class="text-3xl font-bold underline">
+  <div class="bg-"></div>
+  </h1>
+@endsection
